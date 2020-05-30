af820d00dff10f32dc8911fb599b564d447f2c3e
tamari-gray <tamari-gray>
2020-05-31 05:47:22 +1000

    admin choose tagger and start game confirm

--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -31,6 +31,10 @@ class _LobbyState extends State<Lobby> {
 
     final _username = _userData != null ? _userData.username : "";
 
+    final _snackBar = SnackBar(
+      content: Text('choose a tagger'),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Welcome $_username"),
@@ -44,65 +48,109 @@ class _LobbyState extends State<Lobby> {
           ),
         ],
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: _users.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: color,
-                    child: ListTile(
-                      title: tagger == _users[index]
-                          ? Text("${_users[index]} is the tagger")
-                          : Text("${_users[index]} has joined"),
-                      trailing: _userData?.isAdmin == true
-                          ? tagger == _users[index]
-                              ? Icon(Icons.check_box)
-                              : Icon(Icons.check_box_outline_blank)
-                          : null,
-                      selected: tagger == _users[index],
-                      onTap: () {
-                        setState(() {
-                          if (_userData.isAdmin == true) {
-                            if (tagger == _users[index]) {
-                              tagger = "";
-                            } else {
-                              tagger = _users[index];
+      body: Builder(
+        builder: (context) => Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _users.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: color,
+                      child: ListTile(
+                        title: tagger == _users[index]
+                            ? Text("${_users[index]} is the tagger")
+                            : Text("${_users[index]} has joined"),
+                        trailing: _userData?.isAdmin == true
+                            ? tagger == _users[index]
+                                ? Icon(Icons.check_box)
+                                : Icon(Icons.check_box_outline_blank)
+                            : null,
+                        selected: tagger == _users[index],
+                        onTap: () {
+                          setState(() {
+                            if (_userData.isAdmin == true) {
+                              if (tagger == _users[index]) {
+                                tagger = "";
+                              } else {
+                                tagger = _users[index];
+                              }
                             }
-                          }
-                        });
-                      },
-                    ),
-                  );
-                },
+                          });
+                        },
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            _userData.isAdmin == true
-                ? Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 100),
-                    child: RaisedButton(
-                      onPressed: () {
-                        //start game
-                      },
-                      child: Text("start game"),
-                    ),
-                  )
-                : Center(
-                    child: Padding(
+              _userData.isAdmin == true
+                  ? Padding(
                       padding: const EdgeInsets.fromLTRB(0, 50, 0, 100),
-                      child: Text(
-                        "Waitng for more players to join..",
-                        style: TextStyle(fontSize: 22),
+                      child: RaisedButton(
+                        onPressed: () {
+                          tagger == ""
+                              ? Scaffold.of(context).showSnackBar(_snackBar)
+                              : _showMyDialog();
+                        },
+                        child: Text("start game"),
+                      ),
+                    )
+                  : Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 100),
+                        child: Text(
+                          "Waitng for more players to join..",
+                          style: TextStyle(fontSize: 22),
+                        ),
                       ),
                     ),
-                  ),
-          ],
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you ready?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+                    child: RaisedButton(
+                      child: Text("Start game"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text("Cancel"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ])
+          ],
+        );
+      },
     );
   }
 }
