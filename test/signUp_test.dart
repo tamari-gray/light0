36cec4ff7d8ae1285cbe1b64f655c74f1c354a31
tamari-gray <tamari-gray>
2020-06-08 23:44:41 +1000

    setup unit testing (exclamation mark)

---
+++ test/signUp_test.dart
@@ -0,0 +1,13 @@
+// Import the test package and Counter class
+import 'package:test/test.dart';
+import 'package:light0/screens/auth/signUp.dart';
+
+void main() {
+  test('user should only sign up once in firebase', () {
+    final counter = LoginAnon();
+
+    // counter.increment();
+
+    // expect(counter.value, 1);
+  });
+}
